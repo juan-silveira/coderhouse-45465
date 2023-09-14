8555d8fff9f7d8fee6c01912654cab0b720595ce
Juan Silveira <juansilveira@gmail.com>
2023-09-14 19:27:58 -0300

    feat: aula 2 e primeira entrega

---
+++ scripts/aula02.js
@@ -0,0 +1,19 @@
+let nome = "Juan";
+let sobrenome = "Silveira";
+let ano = 2023;
+let idade = 37;
+
+let fullName = nome + " " + sobrenome;
+let anoNascimento = ano - idade;
+
+console.log("Nome completo: ", fullName);
+console.log("Ano nascimento: ", anoNascimento);
+
+//-----------------------------------------
+//Atividade 2
+
+let num1 = parseInt(prompt("Digite o primeiro número:"));
+let num2 = parseInt(prompt("Digite o segundo número:"));
+
+console.log(`O resultado da soma de ${num1} por ${num2} é ${num1 + num2}`);
+alert(`O resultado da multiplicação de ${num1} por ${num2} é ${num1 * num2}`)
