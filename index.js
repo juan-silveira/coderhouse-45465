d5e99fb7897f10e707cb45acd0cba3e903086276
Juan Silveira <juansilveira@gmail.com>
2023-09-28 18:12:49 -0300

    feat: Desafio complementar 02

--- index.js
+++ index.js
@@ -1,57 +1,38 @@
-const addImposto = (valorProduto) => {
-    return valorProduto * 1.6;
-}
-
-const addIcmsSimples = (valorProduto, valorIcms) => {
-    return valorProduto + (valorProduto * (valorIcms / 100));
-}
-
-const baseCalculo = (valorProduto, valorIcms) => {
-    return addImposto(valorProduto) / (1 - (valorIcms / 100));
-}
-
-const calcIcms = (valorProduto, valorIcms) => {
-    let calc = baseCalculo(valorProduto, valorIcms)
-    return  calc * (valorIcms / 100);
-}
-
 const calcularBtn = document.getElementById("calcular");
 calcularBtn.addEventListener("submit", (e) => {
     e.preventDefault();
-    const valorProduto = parseFloat(document.getElementById("valorProduto").value);
-    const valorDolar = () => {
-        if(parseFloat(document.getElementById("valorDolar").value) > 0) {
-            return parseFloat(document.getElementById("valorDolar").value);
-        }
-        return 5;
-    }
-    const valorConvertido = valorProduto / valorDolar();
-    const valorIcms = () => {
-        if(parseFloat(document.getElementById("valorIcms").value) > 0) {
-            return parseFloat(document.getElementById("valorIcms").value);
-        }
-        return 17;
-    }
-    const info1 = document.getElementById("info1");
+    var valor = parseFloat(document.getElementById("valorEmprestimo").value);
+    const select = document.getElementById("selectParcelas").value;
     const resultado = document.getElementById("resultado");
+    var info = document.getElementById("info");
+    info.innerHTML = "";
+    resultado.style.display = "block"
+    switch (select) {
+        case "1":
+            valor = valor * 1.2;
+            info.innerHTML = "<p><b>VALOR TOTAL: R$" + valor.toFixed(2) + "</b></p>";
+            for (i = 0; i < 12; i++) {
+                info.insertAdjacentHTML("beforeend", "<p>Parcela " + (i + 1) + ": " + "R$" + (valor/12).toFixed(2) + "</p>")
+            }
+            break;
 
-    let valor = 0;
-    const valorF = valorProduto.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+        case "2":
+            valor = valor * 1.5;
+            info.innerHTML = "<p><b>VALOR TOTAL: R$" + valor.toFixed(2) + "</b></p>";
+            for (i = 0; i < 24; i++) {
+                info.insertAdjacentHTML("beforeend", "<p>Parcela " + (i + 1) + ": " + "R$" + (valor/24).toFixed(2) + "</p>")
+            }
+            break;
 
-    resultado.style.display = "block";
-    if (valorConvertido > 50) {
-        valor = calcIcms(valorProduto, valorIcms());
-        info1.innerText = `Valor do Produto em Dólar: ${valorConvertido.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}
-        Valor do Produto em Reais: ${valorF}
-        Valor do Imposto (60%): ${(valorProduto * 0.6).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-        Valor do ICMS: ${valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-        Valor Final com imposto e ICMS: ${(addImposto(valorProduto) + valor).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-        Imposto final de: ${(((addImposto(valorProduto) + valor) / valorProduto - 1)*100).toFixed(2)}%`;
-    } else {
-        valor = addIcmsSimples(valorProduto, valorIcms());
-        info1.innerText = `Valor do Produto em Dólar: ${valorConvertido.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}
-        Valor do Produto em Reais: ${valorF}
-        Valor do ICMS (${valorIcms()}%): ${(valor - valorProduto).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}
-        Valor Final com ICMS: ${(valor).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
+        case "3":
+            valor = valor * 2;
+            info.innerHTML = "<p><b>VALOR TOTAL: R$" + valor.toFixed(2) + "</b></p>";
+            for (i = 0; i < 36; i++) {
+                info.insertAdjacentHTML("beforeend", "<p>Parcela " + (i + 1) + ": " + "R$" + (valor/36).toFixed(2) + "</p>")
+            }
+            break;
+
+        default:
+            break;
     }
 })
